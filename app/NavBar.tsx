495698806326cd65d12d15036cdf55f510a6ebfb
chris dany <chrisdany9@gmail.com>
2025-07-18 12:55:13 +0100

    Add classnames dependency and improve NavBar styling

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,12 +1,16 @@
+'use client'
 import Link from 'next/link'
 import React from 'react'
 import { AiFillBug } from "react-icons/ai";
+import { usePathname } from 'next/navigation';
+import classNames from 'classnames';
 
 const NavBar = () => {
   const links = [
     {label: 'Dashboard', href: '/'},
     {label: 'Issues', href: '/issues'},
   ]
+  const currentPath = usePathname();
   return (
       <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
           <Link href="/" className='text-xl'><AiFillBug /></Link>
@@ -14,7 +18,11 @@ const NavBar = () => {
         {links.map((link) => (
           <li key={link.href}>
             <Link href={link.href}
-              className='text-zinc-500 hover:text-zinc-800 transition-colors'>
+              className={classNames({
+                'text-zinc-900': currentPath === link.href,
+                'text-zinc-500': currentPath !== link.href,
+                'hover:text-zinc-800 transition-colors': true
+              })}>
               {link.label}
             </Link>
           </li>
