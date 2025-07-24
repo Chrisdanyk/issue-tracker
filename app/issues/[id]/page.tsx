6d2b4e1931e63b9fdf8f0f9567b7cae03d7fc185
chris dany <chrisdany9@gmail.com>
2025-07-24 22:31:28 +0100

    Refactor issue detail layout by replacing plain text with structured components, including IssueStatusBadge and Card for improved presentation.

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,4 +1,6 @@
+import IssueStatusBadge from '@/app/components/IssueStatusBadge'
 import { prisma } from '@/prisma/client'
+import { Card, Flex, Heading, Text } from '@radix-ui/themes'
 import { notFound } from 'next/navigation'
 import React from 'react'
 
@@ -20,10 +22,14 @@ const IssueDetailPage = async ({ params }: Props) => {
     }
     return (
         <div>
-            <p>{issue?.title}</p>
-            <p>{issue?.description}</p>
-            <p>{issue?.status}</p>
-            <p>{issue?.createdAt.toDateString()}</p>
+            <Heading>{issue?.title}</Heading>
+            <Flex className='space-x-3' my="2">
+                <IssueStatusBadge status={issue?.status} />
+                <Text>{issue?.createdAt.toDateString()}</Text>
+            </Flex>
+            <Card className='mt-5'>
+                <Text>{issue?.description}</Text>
+            </Card>
         </div>
     )
 }
