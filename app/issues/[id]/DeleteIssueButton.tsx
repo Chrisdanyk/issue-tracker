07fd66733213be157043938165e43e352684925c
chris dany <chrisdany9@gmail.com>
2025-08-07 13:08:10 +0200

    Implement confirmation dialog for issue deletion in DeleteIssueButton component, enhancing user experience and preventing accidental deletions.

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { TrashIcon } from '@radix-ui/react-icons'
-import { Button } from '@radix-ui/themes'
+import { AlertDialog, Button, Flex } from '@radix-ui/themes'
 
 
 const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
@@ -13,10 +13,37 @@ const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
         }
     }
     return (
-        <Button color="red">
-            <TrashIcon />
-            Delete
-        </Button>
+        <>
+            <AlertDialog.Root>
+                <AlertDialog.Trigger>
+                    <Button color="red">
+                        <TrashIcon />
+                        Delete
+                    </Button>
+                </AlertDialog.Trigger>
+                <AlertDialog.Content maxWidth="450px">
+                    <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
+                    <AlertDialog.Description size="2">
+                        Are you sure? This action cannot be undone.
+                    </AlertDialog.Description>
+
+                    <Flex gap="3" mt="4" justify="end">
+                        <AlertDialog.Cancel>
+                            <Button variant="soft" color="gray">
+                                Cancel
+                            </Button>
+                        </AlertDialog.Cancel>
+                        <AlertDialog.Action>
+                            <Button variant="solid" color="red">
+                                Delete Issue
+                            </Button>
+                        </AlertDialog.Action>
+                    </Flex>
+                </AlertDialog.Content>
+            </AlertDialog.Root>
+
+
+        </>
     )
 }
 
